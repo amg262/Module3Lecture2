b0ea381c267632d2d13108f3ca4eb9d70fd66895
Andrew <andrewmgunn26@gmail.com>
2021-09-01 12:29:47 -0500

    running jars12 & jars24 in main method

--- Module3Lecture2/Program.cs
+++ Module3Lecture2/Program.cs
@@ -4,18 +4,15 @@ namespace Module3Lecture2
 {
     class Program
     {
-
         static int AskForPurchase(int ounces)
         {
             int count = 0;
             Console.WriteLine($"Would you like to buy {ounces}oz jars? y/n");
             char answer = Console.ReadLine().ToLower()[0];
 
-            // if (answer == 'y') 
-            //     count = GetJarsCount(ounces);
+            if (answer == 'y')
+                count = GetJarsCount(ounces);
 
-            count = (answer == 'y') ? GetJarsCount(ounces);
-            
             return count;
         }
 
@@ -24,19 +21,39 @@ namespace Module3Lecture2
             int count = 0;
             Console.WriteLine($"How many {ounces}oz jars>");
             count = Convert.ToInt32(Console.ReadLine());
-            
+
             return count;
         }
-        
+
         static void Main(string[] args)
         {
-           /*
-            * Create a program for a bee farm
-            * 12 oz jar - $7
-            * 24 ox jar - $13
-            * AskForPurchease (int ounce)
-            * GetJarsCount (int ounces)
-            */
+            /*
+             * Create a program for a bee farm
+             * 12 oz jar - $7
+             * 24 ox jar - $13
+             * AskForPurchease (int ounce)
+             * GetJarsCount (int ounces)
+             */
+
+            int jars12 = 0;
+            int jars24 = 0;
+            double totalJars12 = 0, totalJars24 = 0, total = 0;
+            jars12 = AskForPurchase(12);
+            jars24 = AskForPurchase(24);
+            Console.WriteLine(jars12);
+            Console.WriteLine(jars24);
+
+            if (jars12 > 0)
+            {
+                totalJars12 = jars12 * 7;
+                total += totalJars12;
+            }
+
+            if (jars24 > 0)
+            {
+                totalJars24 = jars24 * 7;
+                total += totalJars24;
+            }
         }
     }
 }
