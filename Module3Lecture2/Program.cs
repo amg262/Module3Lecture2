172c21edb3de7a21a8e52fc94de70a1ce9dda12b
Andrew <andrewmgunn26@gmail.com>
2021-09-01 12:17:54 -0500

    added methods for Jars count and string interpolation for input

--- Module3Lecture2/Program.cs
+++ Module3Lecture2/Program.cs
@@ -4,9 +4,39 @@ namespace Module3Lecture2
 {
     class Program
     {
+
+        static int AskForPurchase(int ounces)
+        {
+            int count = 0;
+            Console.WriteLine($"Would you like to buy {ounces}oz jars? y/n");
+            char answer = Console.ReadLine().ToLower()[0];
+
+            // if (answer == 'y') 
+            //     count = GetJarsCount(ounces);
+
+            count = (answer == 'y') ? GetJarsCount(ounces);
+            
+            return count;
+        }
+
+        static int GetJarsCount(int ounces)
+        {
+            int count = 0;
+            Console.WriteLine($"How many {ounces}oz jars>");
+            count = Convert.ToInt32(Console.ReadLine());
+            
+            return count;
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           /*
+            * Create a program for a bee farm
+            * 12 oz jar - $7
+            * 24 ox jar - $13
+            * AskForPurchease (int ounce)
+            * GetJarsCount (int ounces)
+            */
         }
     }
 }
